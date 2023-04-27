909e1c678ff7b4499d002aaf217d30a1da254094
Edwin H. Jonkvorst <hetlevenkronen@gmail.com>
2023-04-27 11:00:01 +0200

    Update packages.el

--- .emacs.d/init-file-add-ons/packages.el
+++ .emacs.d/init-file-add-ons/packages.el
@@ -243,19 +243,6 @@
 
 
 
-;; --------------------------------------------------------------------------
-;; Disable the arrow, end, home and delete keys, as well as their control and
-;; meta prefixes TODO
-;; --------------------------------------------------------------------------
-(use-package no-easy-keys		; force using the proper Emacs movement keys
-  :if my/modus-tollens
-  :quelpa (no-easy-keys
-	   :fetcher github
-	   :repo "danamlund/emacs-no-easy-keys")
-  :config (no-easy-keys t))
-
-
-
 ;; -----------------
 ;; Disable the mouse
 ;; -----------------
